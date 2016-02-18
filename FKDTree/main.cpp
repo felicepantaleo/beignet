fc8c0d69dde48fc405540120e8e630fb4059c5c7
AdrianoDee <adriano.diflorio@ba.infn.it>
2016-02-18 16:45:31 +0100

    no extern

--- FKDTree/main.cpp
+++ FKDTree/main.cpp
@@ -17,7 +17,7 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
-extern "C" void CUDAKernelWrapper(unsigned int nPoints,float *h_dim,unsigned int *h_ids,unsigned int *h_results);
+void CUDAKernelWrapper(unsigned int nPoints,float *h_dim,unsigned int *h_ids,unsigned int *h_results);
 
 #ifdef OPENCL
 double shrDeltaT()
