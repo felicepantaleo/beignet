a896201b13aea4fbf4e2a58c482908d458c8b855
fpantale <felice.pantaleo@cern.ch>
2016-03-02 02:20:42 +0100

    deinlined to profile

--- FKDTree/FKDTree.h
+++ FKDTree/FKDTree.h
@@ -202,66 +202,9 @@ public:
 		return inTheBox;
 	}
 
-std::vector<unsigned int>  search_in_the_box(
+	std::vector<unsigned int> search_in_the_box(
 			const KDPoint<TYPE, numberOfDimensions>& minPoint,
-			const KDPoint<TYPE, numberOfDimensions>& maxPoint) const
-	{
-//		std::deque<unsigned int> indecesToVisit;
-		FQueue<unsigned int> indecesToVisit(256);
-		std::vector<unsigned int> result;
-		result.reserve(64);
-		indecesToVisit.push_back(0);
-
-		for (int depth = 0; depth < theDepth + 1; ++depth)
-		{
-
-			int dimension = depth % numberOfDimensions;
-			unsigned int numberOfIndecesToVisitThisDepth =
-					indecesToVisit.size();
-			for (unsigned int visitedIndecesThisDepth = 0;
-					visitedIndecesThisDepth < numberOfIndecesToVisitThisDepth;
-					visitedIndecesThisDepth++)
-			{
-
-				unsigned int index = indecesToVisit[visitedIndecesThisDepth];
-//				assert(index >= 0 && index < theNumberOfPoints);
-				bool intersection = intersects(index, minPoint, maxPoint,
-						dimension);
-
-				if (intersection && isInTheBox(index, minPoint, maxPoint))
-					result.push_back(index);
-
-				bool isLowerThanBoxMin = theDimensions[dimension][index]
-						< minPoint[dimension];
-
-				int startSon = isLowerThanBoxMin; //left son = 0, right son =1
-
-				int endSon = isLowerThanBoxMin || intersection;
-
-				for (int whichSon = startSon; whichSon < endSon + 1; ++whichSon)
-				{
-					unsigned int indexToAdd = leftSonIndex(index) + whichSon;
-
-					if (indexToAdd < theNumberOfPoints)
-					{
-
-//						assert(
-//								indexToAdd >= (1 << (depth + 1)) - 1
-//										&& leftSonIndex(index) + whichSon
-//												< ((1 << (depth + 2)) - 1));
-						indecesToVisit.push_back(indexToAdd);
-					}
-
-				}
-
-			}
-
-			indecesToVisit.pop_front(numberOfIndecesToVisitThisDepth);
-//			indecesToVisit.erase(indecesToVisit.begin(),
-//					indecesToVisit.begin() + numberOfIndecesToVisitThisDepth);
-		}
-		return result;
-	}
+			const KDPoint<TYPE, numberOfDimensions>& maxPoint) const;
 
 	bool test_correct_build(unsigned int index = 0, int dimension = 0) const
 	{
@@ -308,7 +251,7 @@ std::vector<unsigned int>  search_in_the_box(
 
 	std::vector<TYPE> getDimensionVector(const int dimension) const
 	{
-		if(dimension < numberOfDimensions)
+		if (dimension < numberOfDimensions)
 			return theDimensions[dimension];
 	}
 
@@ -316,7 +259,6 @@ std::vector<unsigned int>  search_in_the_box(
 	{
 		return theIds;
 	}
-
 
 private:
 
@@ -344,4 +286,65 @@ private:
 
 };
 
+
+std::vector<unsigned int> FKDTree::search_in_the_box(
+		const KDPoint<TYPE, numberOfDimensions>& minPoint,
+		const KDPoint<TYPE, numberOfDimensions>& maxPoint) const
+{
+	//		std::deque<unsigned int> indecesToVisit;
+	FQueue<unsigned int> indecesToVisit(256);
+	std::vector<unsigned int> result;
+	result.reserve(64);
+	indecesToVisit.push_back(0);
+
+	for (int depth = 0; depth < theDepth + 1; ++depth)
+	{
+
+		int dimension = depth % numberOfDimensions;
+		unsigned int numberOfIndecesToVisitThisDepth =
+				indecesToVisit.size();
+		for (unsigned int visitedIndecesThisDepth = 0;
+				visitedIndecesThisDepth < numberOfIndecesToVisitThisDepth;
+				visitedIndecesThisDepth++)
+		{
+
+			unsigned int index = indecesToVisit[visitedIndecesThisDepth];
+			//				assert(index >= 0 && index < theNumberOfPoints);
+			bool intersection = intersects(index, minPoint, maxPoint,
+					dimension);
+
+			if (intersection && isInTheBox(index, minPoint, maxPoint))
+				result.push_back(index);
+
+			bool isLowerThanBoxMin = theDimensions[dimension][index]
+					< minPoint[dimension];
+
+			int startSon = isLowerThanBoxMin; //left son = 0, right son =1
+
+			int endSon = isLowerThanBoxMin || intersection;
+
+			for (int whichSon = startSon; whichSon < endSon + 1; ++whichSon)
+			{
+				unsigned int indexToAdd = leftSonIndex(index) + whichSon;
+
+				if (indexToAdd < theNumberOfPoints)
+				{
+
+					//						assert(
+					//								indexToAdd >= (1 << (depth + 1)) - 1
+					//										&& leftSonIndex(index) + whichSon
+					//												< ((1 << (depth + 2)) - 1));
+					indecesToVisit.push_back(indexToAdd);
+				}
+
+			}
+
+		}
+
+		indecesToVisit.pop_front(numberOfIndecesToVisitThisDepth);
+		//			indecesToVisit.erase(indecesToVisit.begin(),
+		//					indecesToVisit.begin() + numberOfIndecesToVisitThisDepth);
+	}
+	return result;
+}
 #endif /* FKDTREE_FKDTREE_H_ */
